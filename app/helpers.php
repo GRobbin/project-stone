62627f0f4ed6406ce8358abf78feb989486ce7c6
Robin <info@devvv.se>
2018-07-27 21:21:07 +0200

    90% done on models page

--- app/helpers.php
+++ app/helpers.php
@@ -170,9 +170,9 @@ add_action('wp_footer', __NAMESPACE__ . '\\my_deregister_scripts');
 /**
  * Move jQuery to the footer.
  */
-function wpse_173601_enqueue_scripts() {
-    wp_scripts()->add_data('jquery', 'group', 1);
-    wp_scripts()->add_data('jquery-core', 'group', 1);
-    wp_scripts()->add_data('jquery-migrate', 'group', 1);
-}
-add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\wpse_173601_enqueue_scripts');
+// function wpse_173601_enqueue_scripts() {
+//     wp_scripts()->add_data('jquery', 'group', 1);
+//     wp_scripts()->add_data('jquery-core', 'group', 1);
+//     wp_scripts()->add_data('jquery-migrate', 'group', 1);
+// }
+// add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\wpse_173601_enqueue_scripts');
--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
-<div class="page-header">
+{{-- <div class="page-header">
   @if (is_front_page() == 0)
   <h1>{!! App::title() !!}</h1>
   @endif
-</div>
+</div> --}}
